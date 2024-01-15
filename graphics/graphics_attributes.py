40db1d8abf05562e2e010f92f0013f36b43c1b82
Dhjali <barbara.vanni@laplateforme.io>
2024-01-15 11:05:42 +0100

    add input box for user name

--- graphics/graphics_attributes.py
+++ graphics/graphics_attributes.py
@@ -8,5 +8,6 @@ screen = pygame.display.set_mode((WIDTH, HEIGHT))
 
 font_title = pygame.font.Font('./assets/font/Pokemon_Solid.ttf',120)
 font_button_menu = pygame.font.Font('./assets/font/Pokemon_text.ttf',40)
+font_input = pygame.font.Font('./assets/font/Pokemon_text.ttf',40)
 font_long = pygame.font.Font('./assets/font/Pokemon_text.ttf',20)
 font_ingame = pygame.font.Font('./assets/font/Pokemon_text.ttf',15)

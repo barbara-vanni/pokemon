468589e67b8e88eaccb7a19592f3100124789db3
Dhjali <barbara.vanni@laplateforme.io>
2024-01-15 16:37:36 +0100

    add assets

--- graphics/graphics_functions/render_pokemon_choice.py
+++ graphics/graphics_functions/render_pokemon_choice.py
@@ -3,6 +3,14 @@ from graphics.graphics_classes.Button_rect import *
 from graphics.graphics_classes.Button_image import *
 from graphics.graphics_functions.draw_text import *
 
+state = 'pokemon_choices'
+
+def state_pokemon_choices():
+    global num_words
+    num_words = 1
+    water, fire, grass = render_pokemon_choices()
+    return water, fire, grass
+
 def render_pokemon_choices():
     bcg_choice = Image('./assets/images/pokemon_choose.png', (0,0))
     bcg_choice.draw_image(screen)
@@ -15,10 +23,42 @@ def render_pokemon_choices():
     # salameche.draw_image(screen)
     # carapuce = Image('./assets/images/bulbizarre_2.png', (530, 250))
     # carapuce.draw_image(screen)
-    water = Message(20, 300, 200, 50, 'Type Eau', 'black', 'white')
-    water.message_render(font_long, screen)
-    fire = Message(300, 300, 200, 50, 'Type Feu', 'black', 'white')
-    fire.message_render(font_long, screen)
-    grass = Message(580, 300, 200, 50, 'Type Plante', 'black', 'white')
-    grass.message_render(font_long, screen)
+    water, fire, grass = render_pokemon_choices_buttons()
+    # render_pokemon_choices_buttons_image()
+    return water, fire, grass
+
+
+def render_pokemon_choices_buttons():    
+    pokemon_1 = Button_rect(20, 300, 200, 50, 'Type Eau', 'black', 'white')
+    pokemon_1.collision(font_long, screen)
+    pokemon_2 = Button_rect(300, 300, 200, 50, 'Type Feu', 'black', 'white')
+    pokemon_2.collision(font_long, screen)
+    pokemon_3 = Button_rect(580, 300, 200, 50, 'Type Plante', 'black', 'white')
+    pokemon_3.collision(font_long, screen)
+    if pokemon_1.get_clicked():
+        pokemon_1_action()
+    if pokemon_2.get_clicked():
+        pokemon_2_action()
+    if pokemon_3.get_clicked():
+        pokemon_3_action()
+    return pokemon_1, pokemon_2, pokemon_3
+
+
+
+def pokemon_1_action():
+    screen.fill('black')
+    pokemon_1 = Image('./assets/images/caratoji.png', (0, 0))
+    pokemon_1.draw_image(screen)
+
+def pokemon_2_action():
+    screen.fill('black')
+    pokemon_2 = Image('./assets/images/salamoji.png', (0, 0))
+    pokemon_2.draw_image(screen)
+
+def pokemon_3_action():
+    screen.fill('black')
+    pokemon_3 = Image('./assets/images/lufizar.png', (0, 0))
+    pokemon_3.draw_image(screen)
+
+
     
--- main.py
+++ main.py
@@ -5,8 +5,12 @@ from graphics.graphics_functions.render_new_game import *
 from graphics.graphics_functions.render_pokemon_choice import *
 
 continuer = True
+
+
 while continuer:
-    render_pokemon_choices()
+    state_pokemon_choices()
+
+
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             continuer = False

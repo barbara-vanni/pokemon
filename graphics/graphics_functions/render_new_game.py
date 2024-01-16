3399500e067e20f2c6eb59b61d6df9bf81463b36
Dhjali <barbara.vanni@laplateforme.io>
2024-01-16 10:08:29 +0100

    .

--- graphics/graphics_functions/render_new_game.py
+++ graphics/graphics_functions/render_new_game.py
@@ -4,6 +4,7 @@ import json
 from graphics.graphics_attributes import *
 from graphics.graphics_classes.Button_rect import *
 from graphics.graphics_classes.Button_image import *
+from assets import *
 
 def render_new_game():
     bcg_new_game = Image('./assets/images/professor_chen_classico.png', (0, 0))
--- main.py
+++ main.py
@@ -5,14 +5,16 @@ from graphics.graphics_functions.render_new_game import *
 from graphics.graphics_functions.render_pokemon_choice import *
 from event.quit import *
 from event.mouse_button_event import *
-# from game.render_states import *
+
+
+
 
 continuer = True
 
 
 while continuer:
-    # new_game, continu, option = render_main()
-    pokemon_good, nom_good, pv_good, pokemon_bad, nom_bad, pv_bad, attack_button, object_button, flee_button, new_pokemon_button = render_combat()
+    new_game, continu, option = render_main()
+    # pokemon_good, nom_good, pv_good, pokemon_bad, nom_bad, pv_bad, attack_button, object_button, flee_button, new_pokemon_button = render_combat()
     for event in pygame.event.get():
         continuer = quit_event(event, continuer)
         # mouse_button_event(event, new_game, font_ingame, screen)

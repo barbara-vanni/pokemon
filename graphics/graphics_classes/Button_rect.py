31dac4d13361160fb7bff5dcd543fe8ce5f9e2ff
Sluchai40 <mabbolos8@gmail.com>
2024-01-17 10:22:27 +0100

    finish the layer version 0 for pokedex

--- graphics/graphics_classes/Button_rect.py
+++ graphics/graphics_classes/Button_rect.py
@@ -2,21 +2,53 @@
 Creation of Button_rect classes how allow to click on a Message.
 """""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""
 import pygame
+from assets import *
+from graphics.graphics_attributes import font_button_menu
 
-from .Message import *
+class Button():
+    def __init__(self, x, y, text, font, scale, color, hover_color):
+        self.text = text
+        self.font = font_button_menu
+        self.color = color
+        self.hover_color = hover_color
+        self.clicked = False
 
-class Button_rect(Message):
-    def __init__ (self, x, y, width, height, text, color_rect, color_font):
-        Message.__init__(self, x, y, width, height, text, color_rect, color_font)
-        self.__clicked = False
+        self.create_button(x, y, scale)
 
-    def get_clicked(self):
-        return self.__clicked
-    def set_clicked(self):
-        self.__clicked = not self.__clicked
+    def create_button(self, x, y, scale):
+        self.image = self.font.render(self.text, True, self.color)
+        self.rect = self.image.get_rect()
+        self.rect.topleft = (x, y)
 
-    def collision(self, font, screen):
-        self.__collision_rect = self.message_render(font, screen)
+    def draw(self, surface):
+        surface.blit(self.image, (self.rect.x, self.rect.y))
+
+    def check_clicked(self):
+        action = False
         pos = pygame.mouse.get_pos()
-        if self.__collision_rect.collidepoint(pos):
-            self.set_clicked()
+
+        if self.rect.collidepoint(pos):
+            if pygame.mouse.get_pressed()[0] == 1 and not self.clicked:
+                self.clicked = True
+                print (self.text + " Clicked")
+                action = True
+            else:
+                self.image = self.font.render(self.text, True, self.hover_color)
+        else:
+            self.clicked = False
+            self.image = self.font.render(self.text, True, self.color)
+        return action
+    
+    def render(self, screen):
+        self.draw(screen)
+        return self.check_clicked()
+
+
+
+
+#create button instances
+new_game_button = Button(200, 250, "NEW-GAME", font_button_menu, 1, (255, 0, 0), (0, 255, 0))
+continue_button = Button(200, 350, "CONTINUE", font_button_menu, 1, (255, 0, 0), (0, 255, 0))
+option_button = Button(200, 450, "OPTION", font_button_menu, 1, (255, 0, 0), (0, 255, 0))
+attack_button = Button(30, 450, "FIGHT", None, 2, (0, 0, 0), (240, 240, 240))
+# quit_button = Button.Button(200, 100, "Cliquez-moi", None, 2, (255, 0, 0), (0, 255, 0))

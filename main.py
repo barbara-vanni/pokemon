48c2694dcdf8a322f2034dd92f3b8d862f328f67
Sluchai40 <mabbolos8@gmail.com>
2024-01-24 15:23:35 +0100

    last modification before merging dev in

--- main.py
+++ main.py
@@ -2,44 +2,23 @@ import pygame
 from game import *
 from graphics import *
 
-continuer = True
+running = True
 attack_button = None
 
-# while running:
+while running:
 
     # universal event handler ( à mettre seulement ici !)
-    # if pygame.event.Event(pygame.QUIT) in pygame.event.get():
-    #     running = False
+    if pygame.event.Event(pygame.QUIT) in pygame.event.get():
+        running = False
 
     # get_state (who is a function) returns the function to be executed
-    # get_state()()
-    # event handler
-    # for event in pygame.event.get():
-    #     if event.type == pygame.MOUSEBUTTONDOWN:
-    #         # if input_rect.collidepoint(event.pos):
-    #         #     active = True
-    #         # else:
-    #             active = False
-    #     if event.type == pygame.KEYDOWN:
-    #         if active:
-    #             if event.key == pygame.K_BACKSPACE:
-    #                 # get text input from 0 to -1 i.e. end.
-    #                 user_text = user_text[:-1]
-    #             # elif event.key == pygame.K_RETURN:
-    #                 # Save the entered name to the JSON file when Enter key is pressed
-    #                 # save_data(user_text, 0)  
-    #             else:
-    #                 user_text += event.unicode
-    #                 if event.key == pygame.K_SPACE:
-    #                     game_paused = True
-    #     if event.type == pygame.QUIT:
-    #         running = False
+    get_state()()
 
-continuer = True
-attack_button = None
+# continuer = True
+# attack_button = None
 
-while continuer:
-    render_combat_pokemon()
+# while continuer:
+#     render_combat_pokemon()
 #     if get_menu() == 0:
 #         new_game, continu, option = render_main()
 #     elif get_menu() == 1:

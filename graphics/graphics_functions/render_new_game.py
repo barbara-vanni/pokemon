d5b51710f11f79b5b727f12a9c7b333939d1ff01
Sluchai40 <mabbolos8@gmail.com>
2024-01-24 15:15:46 +0100

    modification render_new_game

--- graphics/graphics_functions/render_new_game.py
+++ graphics/graphics_functions/render_new_game.py
@@ -1,4 +1,3 @@
-# import pygame
 from graphics.graphics_attributes import *
 from graphics.graphics_classes import *
 from game.current_render import *
@@ -6,53 +5,72 @@ from graphics.graphics_classes.Button import *
 import game.current_render as Current_render
 from graphics.graphics_functions.render_pokemon_choice import state_pokemon_choices
 
+user_text = ''
+input_rect = pygame.Rect(100, 450, 200, 50)
+active = False
+# color_active = pygame.Color('green')
+# # color of input box
+# color_passive = pygame.Color('red')
+color = (255,245,255)
+
 
 def render_new_game():
+    global active, user_text
     bcg_new_game = Image('./assets/images/professor_chen_classico.png', (0, 0))
     bcg_new_game.draw_image(screen)
     enter_name = Message(300, 100, 600, 60, 'Who are you ?', 'white', 'white')
     enter_name.message_render(font_long, screen)
 
+    input_box_state()
     # Input box for name user
-    input_rect = pygame.Rect(100, 450, 200, 50)
-    input_text = ""
-    font = font_input
-    color = (255,245,255)
-    max_char = 13
-
+    # input_rect = pygame.Rect(100, 450, 200, 50)
+    # color = (255,245,255)
+    # active = False
+    # font = font_input
+    # max_char = 13
+    # input_text = ""
+    # text_surface = font.render(input_text, True, (0,0,0))
+    # width = max(200, text_surface.get_width()+10)
 
 
     for event in pygame.event.get():
         if event.type == pygame.MOUSEBUTTONDOWN:
-            if valider_new_game_button.render(screen):
-                Current_render.set_state(state_pokemon_choices)
-        if event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_RETURN:
-                print(input_text)
-            elif event.key == pygame.K_BACKSPACE:
-                input_text = input_text[:-1]
+            if input_rect.collidepoint(event.pos):
+                active = True
             else:
-                if len(input_text) < max_char:
-                    input_text += event.unicode
-        if event.type == pygame.QUIT:
-            pygame.quit()
+                active = False
+        if event.type == pygame.KEYDOWN:
+            if active:
+                if event.key == pygame.K_BACKSPACE:
+                    # get text input from 0 to -1 i.e. end.
+                    user_text = user_text[:-1]
+                elif event.key == pygame.K_RETURN:
+                    # Save the entered name to the JSON file when Enter key is pressed
+                    print(user_text)
+                    # save_data(user_text, 0)  
+                else:
+                    user_text += event.unicode
+                    if event.key == pygame.K_SPACE:
+                        game_paused = True
+        if valider_new_game_button.render(screen):
+            Current_render.set_state(state_pokemon_choices)
 
-    pygame.draw.rect(screen, color, input_rect) 
-    text_surface = font.render(input_text, True, (0,0,0))
-    width = max(200, text_surface.get_width()+10)
-    input_rect.w = width
-    screen.blit(text_surface, (input_rect.x+5, input_rect.y+5))
     valider_new_game_button.render(screen)
 
 
-    pygame.display.flip()
+def input_box_state():
+    global user_text  
+    # if active:
+    #     color = color_active
+    # else:
+    #     color = color_passive
 
+    # draw rectangle for input box
+    pygame.draw.rect(screen, color, input_rect)
+    text_surface = font_input.render(user_text, True, (0, 0, 0))
+    # render
+    screen.blit(text_surface, (input_rect.x + 5, input_rect.y + 5))
+    # set field for the text so it cannot get outside of the box
+    input_rect.w = max(100, text_surface.get_width() + 10)
 
-
-
-# def save_user_name(input_text):
-#     data = {'user_name': input_text}
-#     with open('user_data.json', 'w') as file:
-#         json.dump(data, file)
-
-
+    return user_text

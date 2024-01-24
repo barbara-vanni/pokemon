363ef005e6c22d382a49742c657f1ae05d096159
Sluchai40 <mabbolos8@gmail.com>
2024-01-24 15:21:31 +0100

    modification of graphics_attributes

--- graphics/graphics_attributes.py
+++ graphics/graphics_attributes.py
@@ -1,5 +1,4 @@
 import pygame
-from assets import *
 
 pygame.init()
 WIDTH = 800
@@ -8,23 +7,25 @@ screen = pygame.display.set_mode((WIDTH, HEIGHT))
 
 font_title = pygame.font.Font('./assets/font/Pokemon_Solid.ttf',120)
 font_title_in_page = pygame.font.Font('./assets/font/Pokemon_Solid.ttf',50)
-font_title_in = pygame.font.Font('./assets/font/Pokemon_Solid.ttf',30)
+font_title_in = pygame.font.Font('./assets/font/Pokemon_text.ttf', 28)
 font_title_water = pygame.font.Font('./assets/font/Pokemon_Solid.ttf',20)
 font_button_menu = pygame.font.Font('./assets/font/Pokemon_text.ttf',40)
+font_button_menu_in = pygame.font.Font('./assets/font/Pokemon_text.ttf',30)
 font_input = pygame.font.Font('./assets/font/Pokemon_text.ttf',40)
 font_long = pygame.font.Font('./assets/font/Pokemon_text.ttf',20)
 font_ingame = pygame.font.Font('./assets/font/Pokemon_text.ttf',15)
+font_little = pygame.font.Font('./assets/font/Pokemon_text.ttf',12)
 
-text_attaque = ("Le Pokémon (nom de pokemon) lance l'attaque (nom de l'attaque). C'est (efficacité) efficace.")
+# text_attaque = ("Le Pokémon (nom de pokemon) lance l'attaque (nom de l'attaque). C'est (efficacité) efficace.")
 text_choix = (f"Hello (user_name), now you're gonna choose your first pokemon . . . . .  Choose wisely !")
 num_words = 0
 
-# menu = 0
-# def get_menu():
-#     return menu
-# def set_menu(new_menu):
-#     global menu
-#     menu = new_menu
+menu = 0
+def get_menu():
+    return menu
+def set_menu(new_menu):
+    global menu
+    menu = new_menu
 
 
 combat = 0
---
+++ graphics/graphics_functions/render_choose_fight.py
@@ -0,0 +1,53 @@
+import pygame
+from game import Pokedex
+from graphics import *
+from graphics.graphics_attributes import *
+import graphics.graphics_functions.render_combat as render_combat
+import game.current_render as Current_render
+from graphics.graphics_functions.render_new_game import *
+import random
+import os
+
+
+
+def render_choose_fight(pokemon1):
+    if get_pokedex_render() == 0:       
+        pokedex = Pokedex()
+       
+
+        image_font_choose_fight = pygame.image.load('./assets/images/background_menu.png')
+        titre_choose_fight = Message(100, 20, 400, 100, 'Ready ?', 'black', 'red')
+        image_trainer_choose_fight = pygame.image.load('./assets/images/ash1.png')
+        
+        pokemon_front_folder = './assets/images/pokemon_front'
+        pokemon_images = [f for f in os.listdir(pokemon_front_folder) if os.path.isfile(os.path.join(pokemon_front_folder, f))]
+        random_pokemon_image = random.choice(pokemon_images)
+        image_random_pokemon = pygame.image.load(os.path.join(pokemon_front_folder, random_pokemon_image))
+        image_random_pokemon = pygame.transform.scale(image_random_pokemon, (300, 400))
+
+
+        screen.blit(image_font_choose_fight, (0, 0))
+        titre_choose_fight.message_render(font_title_in_page, screen)
+
+        if button_one.render(screen):
+            Current_render.set_state(render_new_game)
+        if button_two.render(screen):
+            Current_render.set_state(render_combat)
+        if button_three.render(screen):
+            Current_render.set_state(render_combat)
+        if button_four.render(screen):
+            Current_render.set_state(render_combat.render_combat)
+        if button_five.render(screen):
+            Current_render.set_state(render_combat.render_combat)
+        if button_six.render(screen):
+            Current_render.set_state(render_combat.render_combat)
+        if button_pokedex.render(screen):
+            Current_render.set_state(render_pokedex)
+
+
+        screen.blit(image_trainer_choose_fight, (-10, 180))
+        screen.blit(image_random_pokemon, (200, 160))
+        pygame.display.flip()
+        pygame.time.delay(2000)
+
+     

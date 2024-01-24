239ec8efd06527e5a204e75efd2dd716979deb57
Sluchai40 <mabbolos8@gmail.com>
2024-01-24 15:14:08 +0100

    modification of render_main

--- graphics/graphics_functions/draw_text.py
+++ graphics/graphics_functions/draw_text.py
@@ -27,17 +27,10 @@ def draw_text(screen, text_content, font_long, rectangle, y_begin, y_delta, max_
     y = y_begin
     for line in lines[-max_lines:]:
         text = font_long.render(line, True, 'black')
+        
         text_rect = text.get_rect(center=(rectangle.centerx, y))
-        text.set_clip(text_rect)
         screen.blit(text, text_rect)
         y += y_delta
 
     clock.tick(2)
     num_words += 1
-
-# def erase_text(screen, rectangle, y_begin, y_delta, max_lines):
-#     erase_surface = pygame.Surface((rectangle.width, max_lines * y_delta))
-#     erase_surface.fill((255, 255, 255)) 
-
-#     text_rect = erase_surface.get_rect(center=(rectangle.centerx, y_begin))
-#     screen.blit(erase_surface, text_rect)
